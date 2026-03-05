2e8bbf69a2c62818b05286b0f9b737c9ad69e8fa
kenzomoromizato <kenzo.moromizato@gmail.com>
2026-03-04 21:52:22 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -19,6 +19,7 @@ const Index = () => {
       <ProblemSection />
       <AboutSection />
       <CareModelSection />
+      <WhoIsItForSection />
       <ForWhomSection />
       <DifferentialSection />
       <HowItWorksSection />
