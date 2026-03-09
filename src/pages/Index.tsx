cb0ea001b2abfeea45b5ed28cab886c4292c14b5
kenzomoromizato <kenzo.moromizato@gmail.com>
2026-03-09 10:13:03 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,6 @@ import Navbar from "@/components/landing/Navbar";
 import HeroSection from "@/components/landing/HeroSection";
 import AboutSection from "@/components/landing/AboutSection";
 import CareModelSection from "@/components/landing/CareModelSection";
-import ForWhomSection from "@/components/landing/ForWhomSection";
 import DifferentialSection from "@/components/landing/DifferentialSection";
 import HowItWorksSection from "@/components/landing/HowItWorksSection";
 import ExperienceSection from "@/components/landing/ExperienceSection";
