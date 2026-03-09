a9dbe10bb08059373b1edca5e8d91941f3a7347d
Kenzo Moromizato <kenzo.moromizato@gmail.com>
2026-03-09 14:03:55 -0300

    Add logo to navbar

--- src/components/landing/Navbar.tsx
+++ src/components/landing/Navbar.tsx
@@ -22,7 +22,7 @@ const Navbar = () => {
       <div className="container max-w-6xl mx-auto px-6 flex items-center justify-between">
       <a href="#" className="flex items-center gap-3 bg-white/60 backdrop-blur-sm px-3 py-1 rounded-lg">
   <img
-    src="/morobach-logo.png"
+    src="/morobach-logo-horizontal.png"
     alt="Morobach"
     className="h-14 md:h-16 w-auto object-contain"
   />
