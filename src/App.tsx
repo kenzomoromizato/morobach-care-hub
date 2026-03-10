744ad21b32df3af2daca29aa461ffebe768bd0a2
kenzomoromizato <kenzo.moromizato@gmail.com>
2026-03-10 16:57:10 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,21 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
 import MedicoDeFamiliaBrasilia from "./pages/MedicoDeFamiliaBrasilia";
+import NotFound from "./pages/NotFound";
 
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
+function App() {
+  return (
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Index />} />
-        <Route path="/medico-de-familia-brasilia" element={<MedicoDeFamiliaBrasilia />} />
+        <Route
+          path="/medico-de-familia-brasilia"
+          element={<MedicoDeFamiliaBrasilia />}
+        />
         <Route path="*" element={<NotFound />} />
       </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+    </BrowserRouter>
+  );
+}
 
 export default App;
